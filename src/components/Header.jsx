68bde2dae20c478adda524fb80c47b63eee70b25
CDarts48 <cdartswebdev@gmail.com>
2024-12-27 13:18:00 -0700

    messing with the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './css/Header.css';
+import { Link } from '../../renderer/Link'; // Use your custom Link component
+import './code.css'; // Update the path to the correct CSS file
 
 const Header = () => {
   const [isSubmenuVisible, setIsSubmenuVisible] = useState(false);
@@ -15,21 +15,21 @@ const Header = () => {
       <h1>Topher's Maintenance and Repair</h1>
       <nav>
         <ul className="nav">
-          <li><Link to="/">Welcome</Link></li>
+          <li><Link href="/">Welcome</Link></li>
           <li>
-            <Link to="/services" onClick={toggleSubmenu}>Services</Link>
+            <Link href="/services" onClick={toggleSubmenu}>Services</Link>
             {isSubmenuVisible && (
               <ul className="sub-menu">
-                <li><Link to="/services/landscape-irrigation">Landscape & Irrigation</Link></li>
-                <li><Link to="/services/concrete-asphalt">Concrete & Asphalt</Link></li>
-                <li><Link to="/services/snow-ice">Snow & Ice Management</Link></li>
-                <li><Link to="/services/construction">Construction Services</Link></li>
+                <li><Link href="/services/landscape-irrigation">Landscape & Irrigation</Link></li>
+                <li><Link href="/services/concrete-asphalt">Concrete & Asphalt</Link></li>
+                <li><Link href="/services/snow-ice">Snow & Ice Management</Link></li>
+                <li><Link href="/services/construction">Construction Services</Link></li>
               </ul>
             )}
           </li>
-          <li><Link to="/about-us">About</Link></li>
-          <li><Link to="/reviews">Reviews</Link></li>
-          <li><Link to="/contact-us">Contact</Link></li>
+          <li><Link href="/about-us">About</Link></li>
+          <li><Link href="/reviews">Reviews</Link></li>
+          <li><Link href="/contact-us">Contact</Link></li>
         </ul>
       </nav>
     </header>
